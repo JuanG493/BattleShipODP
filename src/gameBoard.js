1ba3f2b1a5f57b5ca175437210efaeb2af150724
JuanG493 <juanguarin493@gmail.com>
2023-12-08 16:12:09 +0000

    implementing a new form of evaluation the point in the board

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -66,212 +66,8 @@ function setPositionShip(lenghtS,) {
         } else {
             return false
         }
-
     }
-
-
-
 }
-
-
-
-
-function GameBoard() {
-    let list = [];
-    let listUnavailable = [];
-
-    function makeBoard() {
-        for (let i = 11; i < 99; i++) {
-            list[i] = false;
-        }
-        return list;
-    }
-
-    function setStartPoint(size) {
-        let position;
-        //check if it is the first entry, if so then go direcc 
-        if (listUnavailable == 0) {
-            position = getRandomInt();
-            // list[partialPosition] = true;
-            // listUnavailable.push(position);
-            dockyard(position, size);
-
-        } else {
-            let dir;
-            while (true) {
-                position = getRandomInt();
-                // if false, the item can be taken as point of start
-                if (list[position] == false) {
-                    // list[partialPosition] = true;
-
-                    //if true there are at least one direccion to build the ship
-                    dir = direcction(position, size)
-                    if (dir != false) {
-                        break
-                    }
-
-                }
-            }
-
-
-            dockyard(position, size, dir)
-        }
-    }
-
-    // take a point of start, base on the available points and the restriccions of the board
-    // determine a direction to build the ship 
-    // reterun a direccion (top, down, right, left ) or (false) if a point don´t have any valid  direccion
-    function direcction(star, size) {
-        let bondaryLeft
-        let bondaryRight
-
-
-        if (up()) {
-            return 1
-        } else if (right()) {
-            return 2
-        } else if (down()) {
-            return 3;
-        } else if (left()) {
-            return 4;
-        } else {
-            return false;
-        }
-
-        function up() {
-            //check the point and their lef and right, the last one is for the bottom
-            let oneDown = list[star - 10] == false || list[star - 10] == undefined;
-            let oneDownleft = list[(star - 10) - 1] == false || list[(star - 10) - 1] == undefined;
-            let oneDownRigtlist = [(star - 10) + 1] == false || list[(star - 10) + 1] == undefined;
-            if ((checkUpDown('U', star) && oneDown) && ((checkUpDown('u', star - 1) && oneDownleft) || (checkUpDown('U', star + 1)) && oneDownRigtlist)) {
-                return true;
-            } else {
-                return false
-            }
-        }
-        function down() {
-            if (checkUpDown('D', star) && checkUpDown('D', star - 1) && checkUpDown('D', star + 1) && checkUpDown('D', star - 10)) {
-                return true;
-            } else {
-                return false
-            }
-        }
-        function right() {
-
-
-
-
-        }
-        function left() {
-
-        }
-        function checkLeftRoight(params) {
-
-        }
-        function checkOneDoP(stP) {
-
-
-
-        }
-
-        function checkUpDown(polo, pointCheck) {
-            let iter = 0;
-            // check if toda la nave + 1 cabe (+1 porque se necesita que tambiens ese espacio este disponible)
-            for (let i = 0; i < size + 1; i++) {
-                //for the ceil of the ship
-                if (iter == size) {
-                    // if false there are not space up the ship to build this
-                    if (!(list[pointCheck] === false || list[pointCheck] == undefined)) {
-                        iter = 0;
-                        break;
-                    }
-                } else if (list[pointCheck] == false) {
-                    iter++;
-                    if (polo == 'U') {
-                        pointCheck += 10;
-                    } else if (polo == 'D') {
-                        pointCheck -= 10;
-                    } else if (polo == 'L') {
-                        pointCheck - 1;
-                    } else {
-                        pointCheck + 1;
-
-                    }
-                }
-            }
-        }
-    }
-
-
-
-
-
-
-    function dockyard(point, sizeShip, pointer = getRandomInt(1, 4)) {
-        switch (pointer) {
-            case 1: //up
-                while (sizeShip >= 0) {
-                    drawpoinst(point)
-                    point += 10;
-                    sizeShip--;
-                }
-                break;
-            case 2: //right
-                while (sizeShip >= 0) {
-                    drawpoinst(point)
-                    point++;
-                    sizeShip--;
-                }
-                break;
-            case 3: //down
-                while (sizeShip >= 0) {
-                    drawpoinst(point)
-                    point -= 10;
-                    sizeShip--;
-                }
-                break;
-            default: //left
-                while (sizeShip >= 0) {
-                    drawpoinst(point)
-                    point--;
-                    sizeShip--;
-                }
-                break;
-        }
-    }
-    // draw the point and disable all the points around that point
-    function drawpoinst(point) {
-        list[point] = true;
-        // one row before and after
-        // listUnavailable.push(point - 1);
-        list[point - 1] = null;
-        list[point + 1] = null;
-        //one column up and down
-        // listUnavailable.push(point - 10);
-        // listUnavailable.push(point + 10);
-        list[point - 10] = null;
-        list[point - 10] = null;
-        //diagonals -Y
-        // listUnavailable.push((point - 1) - 10)
-        // listUnavailable.push((point - 1) + 10)
-        list[(point - 1) + 10] = null;
-        list[(point - 1) - 10] = null;
-        //diagonals +X 
-        // listUnavailable.push((point + 1) - 10)
-        // listUnavailable.push((point + 1) + 10)
-        list[(point + 1) - 10] = null;
-        list[(point + 1) + 10] = null;
-    }
-    //return a random number between min and max, both included
-    function getRandomInt(min = 11, max = 99) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-
-    makeBoard();
-    return list;
-}
-
-
 // function GameBoard() {
 
 //     let rows = 10;
@@ -323,4 +119,228 @@ function GameBoard() {
 // }
 
 // export { GameBoard, dockyard }
+
+
+
+
+function GameBoard() {
+    let listCoordinates = [];
+    let listUnavailable = [];
+
+    function makeBoard() {
+        for (let i = 0; i <= 99; i++) {
+            listCoordinates[i] = false;
+        }
+        return listCoordinates;
+    }
+    // ####Start HERE####
+    function positionShip(size) {
+        let position;
+        //check if it is the first entry, if so then go direcc 
+        // if (listUnavailable == 0) {
+        // position = getRandomInt();
+        // list[partialPosition] = true;
+        // listUnavailable.push(position);
+        // dockyard(position, size);
+
+        // } else {
+        let dir;
+        while (true) {
+            position = getRandomInt();
+            // if false, the item can be taken as point of start
+            if (listCoordinates[position] == false) {
+                // list[partialPosition] = true;
+
+                //if true there are at least one direccion to build the ship
+                dir = direcction(position, size)
+                if (dir != false) {
+                    break
+                }
+
+            }
+        }
+
+        // send to construction
+        dockyard(position, size, dir)
+
+
+    }
+
+    // take a point of start, base on the available points and the restriccions of the board
+    // determine a direction to build the ship 
+    // reterun a direccion (top 1, down 3, right 2, left 4 ) or (false) if a point don´t have any valid  direccion
+    function direcction(starPoint, sizeShip) {
+        let bondaryLeft
+        let bondaryRight
+
+
+        if (up()) {
+            return 10
+        } else if (right()) {
+            return 1
+        } else if (down()) {
+            return -10;
+        } else if (left()) {
+            return -1;
+        } else {
+            return false;
+        }
+
+        function up() {
+
+            checkPoints(starPoint, sizeShip, 10)
+
+
+
+        }
+        function down() {
+            if (checkUpDown('D', starPoint) && checkUpDown('D', starPoint - 1) && checkUpDown('D', starPoint + 1) && checkUpDown('D', starPoint - 10)) {
+                return true;
+            } else {
+                return false
+            }
+        }
+        function right() {
+
+
+
+
+        }
+        function left() {
+
+        }
+        function checkLeftRoight(params) {
+
+        }
+
+
+        function checkPoints(point, sizeShip, direccion) {
+            let lisForShip = [];
+
+            for (let i = 0; i < sizeShip; i++) {
+                // if and only if the point exist in the board check the perimeter of the point 
+                // if not exist that means it is a wrong direcction
+                if (listCoordinates[point] == null) {
+
+                    if (aux(point) != 0) {
+                        return false;
+                    }
+                    lisForShip.push(point);
+                    if (direccion > 0) {
+                        point += direccion;
+                    } else {
+                        point -= direccion;
+                    }
+                } else {
+                    return false;
+                }
+
+
+            }
+
+            function aux(PontStr) {
+                let total = 8;
+                let cases = [PontStr - 1, PontStr + 1, PontStr - 10, PontStr + 10, (PontStr - 1) + 10, (PontStr - 1) - 10, (PontStr + 1) + 10, (PontStr + 1) - 10]
+
+                cases.forEach(element => {
+                    if (listCoordinates[element] == false || listCoordinates[element] == undefined) {
+                        // here I will check the bondaries for the point ???????????????????
+                        total--;
+                    }
+                });
+                return total;
+            }
+            return true
+        }
+
+
+
+    }
+
+    //point is a number between 10 and 99
+    // true if it´s boudari the point
+    function bondaries(point, comparison) {
+        let row = point % (point % 10);
+        let col = point % 10
+
+
+
+    }
+
+
+
+
+
+    // pointer will be asumme as the direction 
+    function dockyard(point, sizeShip, pointer = getRandomInt(1, 4)) {
+        switch (pointer) {
+            case 1: //up
+                while (sizeShip >= 0) {
+                    drawPoints(point)
+                    point += 10;
+                    sizeShip--;
+                }
+                break;
+            case 2: //right
+                while (sizeShip >= 0) {
+                    drawPoints(point)
+                    point++;
+                    sizeShip--;
+                }
+                break;
+            case 3: //down
+                while (sizeShip >= 0) {
+                    drawPoints(point)
+                    point -= 10;
+                    sizeShip--;
+                }
+                break;
+            default: //left
+                while (sizeShip >= 0) {
+                    drawPoints(point)
+                    point--;
+                    sizeShip--;
+                }
+                break;
+        }
+    }
+    // draw the point and disable all the points around that point
+    function drawPoints(point) {
+        listCoordinates[point] = true;
+        // one row before and after
+        // listUnavailable.push(point - 1);
+        listCoordinates[point - 1] = null;
+        listCoordinates[point + 1] = null;
+
+        //one column up and down
+        // listUnavailable.push(point - 10);
+        // listUnavailable.push(point + 10);
+        listCoordinates[point - 10] = null;
+        listCoordinates[point - 10] = null;
+
+        //diagonals -Y
+        // listUnavailable.push((point - 1) - 10)
+        // listUnavailable.push((point - 1) + 10)
+        listCoordinates[(point - 1) + 10] = null;
+        listCoordinates[(point - 1) - 10] = null;
+
+        //diagonals +X 
+        // listUnavailable.push((point + 1) - 10)
+        // listUnavailable.push((point + 1) + 10)
+        listCoordinates[(point + 1) - 10] = null;
+        listCoordinates[(point + 1) + 10] = null;
+    }
+    //return a random number between min and max, both included
+    function getRandomInt(min = 0, max = 99) {
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+
+    makeBoard();
+    positionShip(4);
+    return listCoordinates;
+}
+GameBoard()
+
+
+
 module.exports = GameBoard;
